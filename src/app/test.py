6b67b3a45bea9eccf2402f3a8a0c6f5701363fc0
Chris Crook <ccrook@linz.govt.nz>
2015-07-08 12:43:51 +1200

    Removing redundant development directory

--- src/app/test.py
+++
@@ -1,7 +0,0 @@
-import LINZ.gazetteer.gui.DatabaseConfiguration
-from LINZ.gazetteer import Export
-
-e = Export.Export()
-# print e.getFields()
-e.createCsvFile('gazetteer_export.gaz_names_csv','gaznames.csv')
-
