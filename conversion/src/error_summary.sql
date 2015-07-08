5ccc00f2d7e4eec77dcac2563d97ff1e337fe00b
Chris Crook <ccrook@linz.govt.nz>
2015-07-08 13:40:43 +1200

    Removing database conversion scripts from public repository - no general value a specific to issues with original data

--- conversion/src/error_summary.sql
+++
@@ -1,8 +0,0 @@
-﻿with t(class, subclass, errcount)  as
-(
-select class, subclass,count(*) from error group by class, subclass
-)
-select class, subclass,errcount, (select error from error e where e.class=t.class and e.subclass=t.subclass limit 1) as example
-from t order by errcount desc;
-
-select * from error;
