c28f0d29143caeaba404a4ea9fb84fa788888625
Sandro Santilli <strk@kbt.io>
2020-03-31 16:45:22 +0200

    Fix docker-compose use of qgis3

--- .docker/qgis/scripts/docker-entry.sh
+++ .docker/qgis/scripts/docker-entry.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+PLUGINDIR=~/.local/share/QGIS/QGIS3/profiles/default/python/plugins/NZGBplugin
+
 # Enable the NZGBplugin in qgis
 /scripts/enable_nzgbplugin || exit 1
 
 # Configure the NZGBplugin
 python3 \
-  ~/.qgis3/python/plugins/NZGBplugin/LINZ/gazetteer/gui/DatabaseConfiguration.py \
+  ${PLUGINDIR}/LINZ/gazetteer/gui/DatabaseConfiguration.py \
   host=${PGHOST} \
   database=${PGDATABASE} \
   user=${PGUSER} \
