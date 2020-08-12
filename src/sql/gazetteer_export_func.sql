020c64ff63bd5db3a7dbd0451088fed0bb4560f5
splanzer <splanzer@linz.govt.nz>
2020-08-13 11:12:57 +1200

    set schema gaz_update_export_database funct

--- src/sql/gazetteer_export_func.sql
+++ src/sql/gazetteer_export_func.sql
@@ -14,7 +14,7 @@
 set search_path=gazetteer_export, gazetteer, public;
 SET client_min_messages=WARNING;
 
-CREATE OR REPLACE FUNCTION gaz_update_export_database()
+CREATE OR REPLACE FUNCTION gazetteer.gaz_update_export_database()
   RETURNS integer AS
 $BODY$
 DECLARE
