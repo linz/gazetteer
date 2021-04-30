34c682fd8e515bc8c07d87869e9bb664c344180f
Bill Nelson <bmnelson@linz.govt.nz>
2021-04-30 19:40:30 +1200

    test: fix typo #248

--- src/sql/test/schema/function_gazetteer.gaz_update_export_database.sql
+++ src/sql/test/schema/function_gazetteer.gaz_update_export_database.sql
@@ -3,7 +3,7 @@ BEGIN;
 SELECT plan(1);
 
 SELECT is(
-     gazetteer.gaz_update_export_database(), 
+     gazetteer.gaz_update_export_database(),
      1,
     'Invoke gaz_update_export_database function'
 );
